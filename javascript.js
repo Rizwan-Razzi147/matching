0ebf855a440d24f4c5b0c44e57b08b1a8d2c6545
Rizwan <rizwandevops147@gmail.com>
2023-05-26 17:04:48 +0500

    matching words javaScript

---
+++ javascript.js
@@ -0,0 +1,25 @@
+// const myWords =["javascript", "course", "laurence"]
+// let player = {}
+// const output1 = document.querySelector('.output1')
+// const output2 = document.querySelector('.output2')
+// const message = document.querySelector('.message')
+// const btn = document.querySelector("button");
+
+// btn.addEventListener("click", function() {
+//     output1.innerHTML = "";
+//     output2.innerHTML = "";
+//     if (myWords.length > 0) {
+//         // btn.style.display = "none"
+//         myWords.sort(function() {
+//             return .5 -Math.random()
+//         })
+//         let theWord = myWords.shift()  
+//         player.solution = theWord.split()      
+//         console.log(player, "asd");
+//         }
+//         else{
+//             message.style.color = "red"
+//             message.innerHTML = "No More Word"
+//         }
+    
+// })
